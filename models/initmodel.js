c7eb9dbe027913b8a53d92cd304eb22abec20a21
Luciano Tito 1801 <lucianotitocedron@gmail.com>
2023-04-24 20:50:35 -0300

    init changes

--- models/initmodel.js
+++ models/initmodel.js
@@ -1,8 +1,13 @@
 const User = require('./users.model');
 const Repair = require('./repair.model');
+
+// Definimos la función que establece la relación entre los modelos
 const initModel = () => {
+  // Establecemos que un usuario puede tener muchas reparaciones
   User.hasMany(Repair, { foreignKey: 'userId' });
+  // Establecemos que una reparación pertenece a un solo usuario
   Repair.belongsTo(User, { foreignKey: 'userId' });
 };
 
+// Exportamos la función para poder usarla en otros archivos
 module.exports = initModel;
